ebac6b1bb06ca435777be6d53dbda7eb58a5b58f
Brendon Posen <brendon.posen@payfast.co.za>
2017-05-04 13:28:20 +0200

    update

--- administrator/components/com_virtuemart/payfast_notify.php
+++ administrator/components/com_virtuemart/payfast_notify.php
@@ -4,7 +4,9 @@
  *
  * PayFast ITN handler
  *
- * @copyright PayFast (Pty) Ltd © 2009-2011
+ * Copyright (c) 2008 PayFast (Pty) Ltd
+ * You (being anyone who is not PayFast (Pty) Ltd) may download and use this plugin / code in your own website in conjunction with a registered and active PayFast account. If your PayFast account is terminated for any reason, you may not use this plugin / code or part thereof.
+ * Except as expressly indicated in this licence, you may not use, copy, modify or distribute this plugin / code or part thereof in any way.
  */
 // Load PayFast configuration file
 define( '_PF_ITN', '1' );    // Define so that CFG file can be included
